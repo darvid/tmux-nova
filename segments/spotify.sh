e3078c26978ad1b69d878778bbad8ea26ccfc65f
David Gidwani <david.gidwani@atomweight.io>
2024-07-31 13:00:26 -0400

    fix: :bug: fix spotify title retrieval on windows

--- segments/spotify.sh
+++ segments/spotify.sh
@@ -9,30 +9,30 @@ source "$(readlink -f $current_dir/../scripts/utils.sh)"
 # https://github.com/erikw/tmux-powerline/blob/main/segments/now_playing.sh#L297
 main() {
   if shell_is_linux; then
-		if type tasklist.exe >/dev/null 2>&1; then  # WSL
-			np="$(tasklist.exe /APPS /fo list /v /fi "IMAGENAME eq Spotify.exe" | grep " - "  | cut -d" " -f3-)"
+    if type pwsh.exe >/dev/null 2>&1; then  # WSL
+      np="$(pwsh.exe -NoProfile -C 'Get-Process | Where-Object { ($_.mainWindowTitle -and $_.Name -eq "Spotify") } | Select -Exp MainWindowTitle')"
       if [[ -z "$np" ]]; then
         np="$(tasklist.exe /fo list /v /fi "IMAGENAME eq Spotify.exe" | grep " - "  | cut -d" " -f3-)"
       fi
-			np="${np//[$'\t\r\n']}"
-		else
-			metadata="$(dbus-send --reply-timeout=42 --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null)"
-			if [ "$?" -eq 0 ] && [ -n "$metadata" ]; then
-				state="$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus'|grep -E -A 1 "string"|cut -b 26-|cut -d '"' -f 1|grep -E -v ^$)"
-				if [[ $state == "Playing" ]]; then
-					artist="$(echo "$metadata" | grep -PA2 "string\s\"xesam:artist\"" | tail -1 | grep -Po "(?<=\").*(?=\")")"
-					track="$(echo "$metadata" | grep -PA1 "string\s\"xesam:title\"" | tail -1 | grep -Po "(?<=\").*(?=\")")"
-					np="$(echo "${artist} - ${track}")"
-				fi
-			fi
-		fi
-	elif shell_is_osx; then
-		np="$(${current_dir}/../scripts/np_spotify_mac.script)"
-	fi
+      np="${np//[$'\t\r\n']}"
+    else
+      metadata="$(dbus-send --reply-timeout=42 --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null)"
+      if [ "$?" -eq 0 ] && [ -n "$metadata" ]; then
+        state="$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus'|grep -E -A 1 "string"|cut -b 26-|cut -d '"' -f 1|grep -E -v ^$)"
+        if [[ $state == "Playing" ]]; then
+          artist="$(echo "$metadata" | grep -PA2 "string\s\"xesam:artist\"" | tail -1 | grep -Po "(?<=\").*(?=\")")"
+          track="$(echo "$metadata" | grep -PA1 "string\s\"xesam:title\"" | tail -1 | grep -Po "(?<=\").*(?=\")")"
+          np="$(echo "${artist} - ${track}")"
+        fi
+      fi
+    fi
+  elif shell_is_osx; then
+    np="$(${current_dir}/../scripts/np_spotify_mac.script)"
+  fi
   if [ -z "$np" ]; then
     np="Not playing"
   fi
-	echo "$(get_segment_content $SEGMENT_NAME "$np")"
+  echo "$(get_segment_content $SEGMENT_NAME "$np")"
 }
 
 main "$@"
--- segments/window-name.sh
+++ segments/window-name.sh
@@ -1,85 +1,66 @@
 #!/bin/bash
 # https://github.com/joshmedeski/tmux-nerd-font-window-name
 
-
-tmux_options="$(tmux show -g)"
-
-get_tmux_option() {
-  local option="$1"
-  local default_value="$2"
-  local option_value="$(echo "$tmux_options" | grep "^$option " | cut -d " " -f2-)"
-  [[ "$option_value" == "''" ]] && [[ -z "$default_value" ]] && echo "" || echo "$default_value"
-}
-
 name=$1
 active=${2:false}
-show_name="$(get_tmux_option '@nova-pane-show-window-name')"
-name_colors="$(get_tmux_option '@nova-pane-name-colors')"
-icon_colors="$(get_tmux_option '@nova-pane-icon-colors')"
-show_inactive_divider="$(get_tmux_option '@nova-pane-show-inactive-divider')"
-divider="$(get_tmux_option '@nova-pane-divider' '')"
-divider_colors="$(get_tmux_option '@nova-pane-divider-colors')"
-divider_colors_active="$(get_tmux_option '@nova-pane-divider-active-colors')"
-
-function get_shell_icon() {
-	local default_shell_icon=""
-	local shell_icon="$(tget_tmux_option '@nova-pane-window-name-shell-icon')"
-	if [ -n "$shell_icon" ]; then
-		echo "$shell_icon"
-	else
-		echo "$default_shell_icon"
-	fi
-}
+show_name=true
+name_colors="fg:#313244"
+icon_colors="fg:magenta"
+show_inactive_divider="false"
+divider=""
+divider_colors="fg:#f2cdcd"
+divider_colors_active="fg:#f2cdcd"
+default_shell_icon=""
 
 get_icon() {
-	case $name in
-	tmux)
-		echo ""
-		;;
-	htop | top)
-		echo ""
-		;;
-	fish | zsh | bash | tcsh)
-		echo "$(get_shell_icon)"
-		;;
-	vi | vim | nvim | lvim)
-		echo ""
-		;;
-	lazygit | git | tig)
-		echo ""
-		;;
-	node)
-		echo ""
-		;;
-	ruby)
-		echo ""
-		;;
-	go)
-		echo "ﳑ"
-		;;
-	lf | lfcd)
-		echo ""
-		;;
-	beam | beam.smp) # Erlang runtime
-		echo ""
-		;;
-	rustc | rustup)
-		echo ""
-		;;
-	python)
-		echo ""
-		;;
-	docker)
-		echo "󰡨"
-		;;
-	*)
-		echo "󰊠"
-		;;
-	esac
+  case $name in
+  tmux)
+    echo ""
+    ;;
+  htop | top)
+    echo ""
+    ;;
+  fish | zsh | bash | tcsh)
+    echo "$default_shell_icon"
+    ;;
+  vi | vim | nvim | lvim)
+    echo ""
+    ;;
+  lazygit | git | tig)
+    echo ""
+    ;;
+  node)
+    echo ""
+    ;;
+  ruby)
+    echo ""
+    ;;
+  go)
+    echo "ﳑ"
+    ;;
+  lf | lfcd)
+    echo ""
+    ;;
+  beam | beam.smp) # Erlang runtime
+    echo ""
+    ;;
+  rustc | rustup)
+    echo ""
+    ;;
+  python)
+    echo ""
+    ;;
+  docker)
+    echo "󰡨"
+    ;;
+  *)
+    echo "󰊠"
+    ;;
+  esac
 }
 
 main() {
-  local icon="#{?window_activity_flag,,$(get_icon)}"
+  local icon="\r#{?window_activity_flag,,$(get_icon)}"
   local _divider
 
   if [ "${show_name:-true}" = true ]; then
@@ -90,9 +71,9 @@ main() {
       _divider=" "
       local divider_colors="$divider_colors"
     fi
-    echo "#[$icon_colors]$icon" "#[$divider_colors]$_divider" "#[$name_colors]$name"
+    echo -e "#[$icon_colors]$icon" "#[$divider_colors]$_divider" "#[$name_colors]$name"
   else
-    echo "#[$icon_colors]$icon"
+    echo -e "#[$icon_colors]$icon"
   fi
 }
 
